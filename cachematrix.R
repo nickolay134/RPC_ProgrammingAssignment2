e39f4393e470343eafdcaf908712be47d02affc2
Nikolay M <nikolay@live.ca>
2020-08-10 20:05:56 -0400

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,41 @@
-## Put comments here that give an overall description of what your
-## functions do
+## This is a brief description for the Week 3 Peer Review Assignment
+## R Programming Course
 
-## Write a short comment describing this function
+## makeCacheMatrix is a function that returns a list of functions
+## It serves to store a martix and cached value of its inverse 
+## The content of the function is as follows:
+## > setMatrix      sets the value of a matrix
+## > getMatrix      gets the value of a matrix
+## > cacheInverse   gets the cahced value (inverse of the matrix)
+## > getInverse     gets the cahced value (inverse of the matrix)
+
 
 makeCacheMatrix <- function(x = matrix()) {
-
+        inv <- NULL
+        set <- function(y) {
+                x <<- y
+                inv <<- NULL
+        }
+        get <- function() {x}
+        setInverse <- function(inverse) {inv <<- inverse}
+        getInverse <- function() {inv}
+        list(set - set, get = get, setInverse = setInverse, getInverse = getInverse)
 }
 
-
-## Write a short comment describing this function
+## The function below computes the inverse of a matrix returned by 
+## the function above 
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+        inv <- x$getInverse()
+        if(!is.null(inv)){
+                message("getting cached data")
+                return(inv)
+        }
+        mat <- x$get()
+        inv <- solve(mat,...)
+        x$setInverse(inv)
+        inv
 }
+
+## Given the assumptions of the assignment, 
+## the matrix supplied is always invertible 
